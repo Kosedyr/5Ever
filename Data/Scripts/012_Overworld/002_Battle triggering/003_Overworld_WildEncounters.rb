23e7da5dc4660e57fa14fcb924c8ceaaae345c04
Jeeppinger <33111071+Jeeppinger@users.noreply.github.com>
2023-01-17 17:51:13 -0500

    catchup with 4ever
    catchup with 4ever

--- Data/Scripts/012_Overworld/002_Battle triggering/003_Overworld_WildEncounters.rb
+++ Data/Scripts/012_Overworld/002_Battle triggering/003_Overworld_WildEncounters.rb
@@ -266,11 +266,11 @@ class PokemonEncounters
         ret = :BugContest if pbInBugContest? && has_encounter_type?(:BugContest)
         ret = find_valid_encounter_type_for_time(:Land, time) if !ret
       end
-      if !ret && has_cave_encounters?
-        ret = find_valid_encounter_type_for_time(:Cave, time)
-      end
       if !ret && has_puddle_encounters? && $game_map.terrain_tag($game_player.x, $game_player.y).puddle_wild_encounters
         ret = find_valid_encounter_type_for_time(:Puddle, time)
+      end
+      if !ret && has_cave_encounters?
+        ret = find_valid_encounter_type_for_time(:Cave, time)
       end
     end
     return ret
